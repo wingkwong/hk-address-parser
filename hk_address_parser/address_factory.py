415a00238f0383838863807ee0150f3a3dd8a8a7
wingkwong <wingkwong.code@gmail.com>
2020-01-15 21:09:00 +0800

    feat: add AddressFactory

---
+++ hk_address_parser/address_factory.py
@@ -0,0 +1,18 @@
+from .addresses import OGCIOAddress, LandAddress, Address
+import hk_address_parser.constant as constant
+
+class AddressFactory:
+    def __init__(self, source, record):
+        self.source = source
+        self.record = record
+    
+    def create_address(self):
+        source = self.source
+        record = self.record
+
+        if source == constant.SOURCE_OCGIO:
+            return OGCIOAddress(record)
+        elif source == constant.SOURCE_LAND:
+            return LandAddress(record)
+        else:
+            return Address(record)
